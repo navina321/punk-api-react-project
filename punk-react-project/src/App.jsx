e8063000dcb92e7a48e59cc142564bebd1dc2d43
navina321 <81630548+navina321@users.noreply.github.com>
2023-02-09 16:35:35 +0000

    added containers & components folders and scss to visualize containers

---
+++ punk-react-project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.scss';
+import Main from './containers/Main/Main';
+import NavBar from './containers/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <NavBar/>
+      </section>
+
+      <section>
+        <Main/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
